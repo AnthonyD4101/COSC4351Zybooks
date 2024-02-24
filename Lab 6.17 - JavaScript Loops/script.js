31c59ed751a61226bd1ac448b0481a502cc429db
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-02-24 00:50:34 -0600

    passed test cases

---
+++ Lab 6.17 - JavaScript Loops/script.js
@@ -0,0 +1,9 @@
+function drawTriangle(triangleSize) {
+   let line = '';
+
+   // Your solution goes here
+   for(let i = 1; i < triangleSize + 1; i++){
+      line += '*';
+      console.log(line);
+   }
+}
